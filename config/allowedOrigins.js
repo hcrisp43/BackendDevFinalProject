02623b043fbf7f9258f0d7980990b718965bdc79
Hunter Crisp <hcrisp43@gmail.com>
2022-05-09 17:52:30 -0500

    Update allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     //'https://www.yoursite.com',
+    'https://huntercrisp-backend-finalproject.glitch.me/',
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3500'
 ];
